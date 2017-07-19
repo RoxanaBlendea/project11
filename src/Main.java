cd26a7e4b0e15b155184a870c868554d91d84bc8
RoxanaBlendea <roxana.blendea@ulbsibiu.ro>
2017-07-19 14:06:24 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        int a=0;
+        a=a+2;
+        a=a+4;
+        int b=0;
+        b=a/6;
+
+        System.out.println(a+ "\n" +b);
+
+    }
+}
